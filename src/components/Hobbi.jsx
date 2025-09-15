f925b6c26898e45cc15c1e72bb7057e9e86026a6
Tóth Attila <toth.attila.02@szte.hu>
2025-09-15 12:11:03 +0200

    init repo

---
+++ src/components/Hobbi.jsx
@@ -0,0 +1,13 @@
+function Hobbi(){
+  return (
+    <>
+      <h2>Kedvenc időtöltéseim</h2>
+      <ul>
+        <li>foci</li>
+        <li>olvasás</li>
+        <li>filmnézés</li>
+      </ul>
+    </>
+  )
+}
+export default Hobbi
---
+++ src/components/Konyvek.jsx
@@ -0,0 +1,9 @@
+function Konyvek(){
+  return (
+    <>
+      <h2>Kedvenc könyveim</h2>
+      
+    </>
+  )
+}
+export default Konyvek
